5d95e6bb45a17f86a2a0a2fe4ebd559498b6679b
dlcm19 <dlcm1308@gmail.com>
2026-02-16 08:12:12 -0500

    Formulario de contacto con email,  WhatsApp y menú

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const carousel = document.getElementById("carousel");
 const nextBtn = document.getElementById("nextBtn");
 const prevBtn = document.getElementById("prevBtn");
@@ -9,3 +10,18 @@ nextBtn.addEventListener("click", () => {
 prevBtn.addEventListener("click", () => {
     carousel.scrollBy({ left: -300, behavior: "smooth" });
 });
+
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+
+console.log("JS cargado");
+
+console.log(menuToggle);
+console.log(navLinks);
